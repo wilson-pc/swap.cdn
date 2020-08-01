e7b9f5ea9b56690532a3dfdf4984851f9a15f817
wilson-pc <wilverhidalgo@outlook.com>
2020-08-01 11:13:18 -0400

    Update index.js
    chnage port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var app = require("./src/app");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-const PORT = process.env.PORT || 3001
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(server_port, server_ip_address, () => {
     console.log(`Servidor de Imagenes puerto:${PORT}`);
 })
